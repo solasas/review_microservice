0f36685f8efe47f4512bb5971a62021ab7d825f5
sashank solasa <sashanksolasa@gmail.com>
2026-05-28 00:51:57 +0530

    after resolving all dependency related issues after configuring the service registry

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+-- Insert sample reviews
+INSERT INTO review (company_id, rating, title, description) VALUES
+(1, 5, 'Great company to work for', 'Excellent work culture and growth opportunities'),
+(1, 4, 'Good experience', 'Nice team and supportive management'),
+(2, 4, 'Innovative workplace', 'Great projects and learning opportunities'),
+(2, 3, 'Decent experience', 'Good pay but demanding projects'),
+(3, 5, 'Top-notch company', 'Amazing data science team and resources');
+
